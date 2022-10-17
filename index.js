d033c43c0cb73a916c037650e4d24e7103b08d4b
Arpita Roy <arpita.roy.workar@gmail.com>
2022-10-17 19:14:23 +0530

    Add addeventListner ("mouseenter")

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -10,6 +11,22 @@ setTimeout(function(){
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
+
+/*
+Challenge:
+1. Take control of the decline btn. (What do you
+   need to do to make that possible?)
+2. Set up an eventListener that is triggered
+   when a user's cursor hovers over the decline
+   button. (Google the event to listen out for!)
+3. For now, just log out "hovered" when that
+   happens.
+*/
+
+declineBtn.addEventListener('mouseenter', function(){
+    console.log('hovered')
+})
+
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
@@ -28,12 +45,6 @@ consentForm.addEventListener('submit', function(e){
         Making the sale...`
     }, 1500)
 
-/*
-Challenge:
-1. Make the button that closes the modal disabled.
-2. Make that button become usable when the final
-   modal message has been displayed to the user.
-*/
 
     setTimeout(function(){
         document.getElementById('modal-inner').innerHTML = `
